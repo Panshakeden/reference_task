40d18901b19c1b0e605a0455915cf9ee6bc887a8
panshak <panshakeden@gmail.com>
2024-06-13 22:08:44 +0100

    rust:task one

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+fn main(){
+
+    print_console();
+}
+
+
+
+//Creating a dangling reference
+
+// fn dangling_fn() -> &string {
+
+//     let dangling_string = String::from("Rustaceans");
+
+//     &dangling_string
+// }
+
+//creating function that accept mutable reference
+
+fn mutable_reference(my_state:&mut String){
+
+    let mut my_state= String::from("Bauchi");
+
+    let current_state= &mut my_state;
+
+}
+
+// print on console
+
+fn print_console(){
+    println!("Let's get rusty in the console");
+}
